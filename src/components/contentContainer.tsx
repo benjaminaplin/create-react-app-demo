0ecb680c48641af9156f91a3edd03d5efee5b40b
Benjamin Aplin <benjaminaplin@gmail.com>
2021-10-11 17:28:15 -0500

    updates

--- src/components/contentContainer.tsx
+++ src/components/contentContainer.tsx
@@ -3,12 +3,18 @@ import { AxiosResponse } from 'axios'
 import { PaintingList } from '../components/paintingList'
 import service from '../service'
 type AppContentProps = {}
-
+export type Painting = {
+  id: string,
+  name: string,
+  description: string,
+  price: number
+}
 export const AppContent = ({}: AppContentProps) => {
-  const [paintings, setPaintings ] = useState<string[]>([])
+  const [paintings, setPaintings ] = useState<Painting[]>([])
   
   useEffect(() => {
-    service.getPaintings().then(({data}: AxiosResponse<{paintings: string[]}>) => {
+    service.getPaintings().then(({data}: AxiosResponse<{paintings: Painting[]}>) => {
+      console.log("🚀 ~ file: contentContainer.tsx ~ line 18 ~ service.getPaintings ~ data", data)
       setPaintings(data.paintings)
     }).catch(err => console.error('whoops', err))
   }, [])
--- src/components/paintingList.tsx
+++ src/components/paintingList.tsx
@@ -2,29 +2,27 @@ import React from 'react'
 import { Typography } from 'antd';
 const { Title } = Typography
 import { Card } from 'antd';
-
 const { Meta } = Card;
+import type { Painting } from './contentContainer'
 
+type PaintingListProps = { paintings: Painting[] }
 
-type PaintingListProps = {paintings: string[]}
-
-export const PaintingList = ({paintings}: PaintingListProps) => {
+export const PaintingList = ({paintings}: PaintingListProps) => { 
 console.log("🚀 ~ file: paintingList.tsx ~ line 12 ~ PaintingList ~ paintings", paintings)
   return (
     <div>
-      {paintings.map((painting: string) => {
-        
+      {paintings?.map((painting: Painting) => {
         return (
-          <div key={painting}>
+          <div key={painting.id}>
             <Title>
-            {painting}
+            {painting.name}
             </Title>
             <Card
               hoverable
               style={{ width: 240 }}
               // cover={<img alt="example" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />}
             >
-              <Meta title={painting} description={painting} />
+              <Meta title={painting.description} description={painting.price} />
             </Card>
           </div>
           )})}
